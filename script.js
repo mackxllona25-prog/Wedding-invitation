19cc21f38e2ac48c841f8933220897606a3c06b9
mackxllona25-prog <mackx.llona25@gmail.com>
2026-07-28 20:37:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,14 @@ section.classList.add("show");
 window.addEventListener("scroll",reveal);
 
 reveal();
+
+// ===== GALLERY LIGHTBOX =====
+
+function openImage(src){
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = src;
+}
+
+function closeImage(){
+    document.getElementById("lightbox").style.display = "none";
+}
