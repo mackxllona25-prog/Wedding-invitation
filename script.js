79de873ba02f5493271fefa9bd03d5a3954ced2e
mackxllona25-prog <mackx.llona25@gmail.com>
2026-07-30 19:25:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,18 @@
-const weddingDate = new Date("April 27, 2027 15:00:00").getTime();
+const btn = document.getElementById("enterBtn");
+const opening = document.getElementById("opening-screen");
+const website = document.getElementById("website");
 
-const timer = document.getElementById("timer");
+btn.addEventListener("click", ()=>{
 
-function countdown(){
+    opening.classList.add("open");
 
-const now = new Date().getTime();
-
-const distance = weddingDate-now;
-
-if(distance<0){
-timer.innerHTML="💍 Today is our Wedding Day!";
-return;
-}
-
-const days=Math.floor(distance/(1000*60*60*24));
-const hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
-const minutes=Math.floor((distance%(1000*60*60))/(1000*60));
-const seconds=Math.floor((distance%(1000*60))/1000);
-
-timer.innerHTML=
-days+" Days<br>"+
-hours+" Hours<br>"+
-minutes+" Minutes<br>"+
-seconds+" Seconds";
-
-}
-
-countdown();
-
-setInterval(countdown,1000);
-
-
-// Fade animation
-
-const sections=document.querySelectorAll("section");
-
-function reveal(){
-
-sections.forEach(section=>{
-
-const top=section.getBoundingClientRect().top;
-
-if(top<window.innerHeight-120){
-
-section.classList.add("show");
-
-}
+    setTimeout(()=>{
+        opening.style.display="none";
+        website.style.display="block";
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        });
+    },2000);
 
 });
-
-}
-
-window.addEventListener("scroll",reveal);
-
-reveal();
-
-// ===== GALLERY LIGHTBOX =====
-
-function openImage(src){
-    document.getElementById("lightbox").style.display = "flex";
-    document.getElementById("lightbox-img").src = src;
-}
-
-function closeImage(){
-    document.getElementById("lightbox").style.display = "none";
-}
